2b6278e88f11046195dbab77753f3045de9c149c
urquico <kurtjacob.urquico@gmail.com>
2025-10-03 19:14:02 +0800

    setup: initial commit

---
+++ src/main/java/com/example/covault/CovaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.covault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/covault/CovaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.covault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
